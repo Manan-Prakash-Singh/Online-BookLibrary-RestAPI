65d31c7867264aa7bb7b8e10170b17b9186526b3
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-22 22:52:26 +0530

    added user table

--- models/user.go
+++ models/user.go
@@ -8,3 +8,21 @@ type User struct {
 	Password  string `json:"password"`
 	IsAdmin   bool   `json:"is_admin"`
 }
+
+func CreateUser(user *User) error {
+
+	_, err := db.Exec(`INSERT INTO users(first_name,last_name,email,password,is_admin) VALUES(
+        $1, $2, $3, $4, $5)`,
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		user.Password,
+		user.IsAdmin,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
